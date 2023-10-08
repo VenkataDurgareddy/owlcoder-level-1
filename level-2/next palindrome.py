876c1e38bb36eb29fae2ca486abc82b9182d737c
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2023-10-08 17:14:45 +0530

    next palindrome.py

---
+++ level-2/next palindrome.py
@@ -0,0 +1,16 @@
+def palin(x):
+    v = x
+    s = 0
+    while(v!=0):
+        d = v%10
+        s = (s*10)+d 
+        v = v//10
+    if s == x:
+        return True
+    else:
+        return False
+n = int(input())
+for i in range(n+1,(10**9)+1):
+    if palin(i):
+        print(i)
+        break
