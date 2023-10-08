676bd3920d4d60465a932479e9df0ff34bba5c42
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2023-10-08 15:47:02 +0530

    Bike booking.py

---
+++ level-2/Bike booking.py
@@ -0,0 +1,10 @@
+N=int(input())
+pos=int(input())
+bike=list(map(int,input().split()))
+time_ta=list(map(int,input().split()))
+mine=9999999999999
+for i in range(N):
+    dis=abs(pos-bike[i])
+    ti=dis*time_ta[i]
+    mine=min(mine,ti)
+print(mine)
