0dfe771ac036422b3453ca5c63a875207b2f1891
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2023-10-08 10:18:34 +0530

    strong element.py

---
+++ level-2/strong element.py
@@ -0,0 +1,20 @@
+n=int(input())
+l=list(map(int ,input().split()))
+p=[]
+for i in set(l):
+    p.append(l.count(i))
+m=max(p)
+c=0
+res=0
+for i in set(l):
+    if(l.count(i)==m):
+        c=c+1
+        res=i
+if(c==1):
+    print("Yes")
+    print(res)
+else:
+    print("No")
+
+
+
