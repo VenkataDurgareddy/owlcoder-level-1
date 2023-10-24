e4a77a0f77da0e0a397f2bdc2314d3a035bc8ec8
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2023-10-24 15:24:35 +0530

    Binary array to decimal.cpp

---
+++ level-3/Binary array to decimal.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=0;
+    int j=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        ans=ans+(arr[i]*pow(2,j));
+        j++;
+    }
+    cout<<ans;
+}
