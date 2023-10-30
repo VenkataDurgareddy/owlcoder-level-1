00b807bc9fe71b779289b5309e3a7eb2dcbde7ec
Satti Venkata Durga Reddy <97235269+VenkataDurgareddy@users.noreply.github.com>
2023-10-30 19:45:06 +0530

    Find the time.cpp

---
+++ level-3/Find the time.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    int time_together = 1 / (3.0 / x);
+    
+    cout << time_together << endl;
+    
+    return 0;
+}
